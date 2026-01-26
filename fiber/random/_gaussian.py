0687572c2654230820c4ccf77019c0100d40f896
Evan Palmer <evanp922@gmail.com>
2026-01-26 11:04:23 -0800

    starting to add gaussians

--- fiber/random/_gaussian.py
+++ fiber/random/_gaussian.py
@@ -17,3 +17,50 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+from functools import singledispatch
+
+import jax
+import jax.numpy as jnp
+from jaxtyping import Array, PRNGKeyArray
+
+from .._elements import Isometry, Twist
+from .._elements._twist import _from_vector
+from .._operations import expm
+
+
+def _sample_algebra(key: PRNGKeyArray, num_samples: int | None = None) -> Array:
+    shape = (num_samples,) if num_samples is not None else None
+    return jax.random.multivariate_normal(key, jnp.zeros(6), jnp.eye(6), shape)
+
+
+@singledispatch
+def left_gaussian(key, mean, num_samples):
+    return _left_gaussian(key, mean, num_samples)
+
+
+def _left_gaussian(
+    key: PRNGKeyArray, mean: Array, num_samples: int | None = None
+) -> tuple[Array, Array]:
+    s = _sample_algebra(key, num_samples)
+    gs = jax.vmap(lambda x: mean @ expm(x))(s)
+    vs = _from_vector(s)
+    return gs, vs
+
+
+@left_gaussian.register
+def _left_gaussian_type(
+    key: PRNGKeyArray, mean: Isometry, num_samples: int | None = None
+) -> tuple[Isometry, Twist]:
+    gs, vs = _left_gaussian(key, mean.coordinates, num_samples)
+    return Isometry.from_matrix(gs), Twist.from_vector(vs)
+
+
+@jaxtyped(typechecker=beartype)
+def right_gaussian(
+    key: PRNGKeyArray, mean: Num[Array, "n n"], num_samples: int
+) -> tuple[Num[Array, "m n n"], Num[Array, "m n n"]]:
+    vels = _sample_lie_algebra(key, num_samples)
+    gs = jax.vmap(lambda ξ: jax.scipy.linalg.expm(ξ) @ mean)(vels)
+    g_circs = jax.vmap(to_matrix)(vels)
+    return gs, g_circs
